ebd20863dc6d09608679b9d1ef643d0171efe38b
Andrei Calin <andrei.calin@toptal.com>
2022-06-16 02:03:41 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   resources :answers
   resources :questions
   resources :students
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "questions#index"
+  root "pages#home"
 end
