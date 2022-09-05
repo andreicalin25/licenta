cbfdab8fe0caa15afe8cbddd2f0608f838470367
Andrei Calin <andrei.calin@toptal.com>
2022-09-06 02:04:08 +0300

    De acum incolo testez si nu stiu daca merge

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "jquery_ujs"
 import "@popperjs/core"
 import "bootstrap"
 import "custom/main"
@@ -20,5 +22,4 @@ import "custom/main"
 // = require jquery
 // = require jquery_ujs
 // = require_tree .
-// = require popper
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
 pin "my_scripts"
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0/dist/js/bootstrap.esm.js", preload: true
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js", preload: true
